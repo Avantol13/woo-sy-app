0bf3126a8bd01f9e614f99673c8cc612aa91cd8c
Alex <avantol13@gmail.com>
2018-09-22 19:17:49 -0500

    fix incorrect var name

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from app import login
 
 @login.user_loader
 def load_user(user_id):
-    return User.query.get(int(id))
+    return User.query.get(int(user_id))
 
 
 class User(UserMixin, db.Model):
